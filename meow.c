06234ff159ac60e4a686bea2082fe64467a5e98a
TahaShrif <taha14736923@gmail.com>
2022-10-14 13:08:28 +0200

    commit on friday

---
+++ meow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{ //This is using a while loop
+    int i = 0;
+    while (i <= 5)
+    {
+        printf("Meow\n");
+        i += 1;
+    }
+}
+
+int main(void)
+{
+    int i = 17;
+    
+}
