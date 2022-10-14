16d90fd2247dec595946dfe2e0335239e95c5ad0
TahaShrif <taha14736923@gmail.com>
2022-10-14 14:01:58 +0200

    Notes on placeholders
    Tested the printf() function as well placeholders too see how exactly do they work

--- remainder.c
+++ remainder.c
@@ -3,6 +3,18 @@
 int main(void)
 {
     int value = 13%2;
-    printf("%i The remainder of the equation 13/2 is equal to: ",value);
+    // printf("The remainder of the equation 13/2 is equal to: %i ",value);
+
+    // printing an int variable
+    int i1 = 1;
+    int i2 = 2;
+    int i3 = 3;
+    char LetA = 'A';
+    float ie = 12.2;
+    printf("i1 = %i, i2 = %i, Let A is: %c", i1, i2, LetA);
+    //Notes:
+    //  The order of placeholders and variables written after the
+    //  is what determines what will be where
+    
 }
-// hello this is a comment which is like a comment which changes nothing in the code 
+
