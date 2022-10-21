da2430d5ac581c12454d1a8af2c1e88a8f33797f
TahaShrif <taha14736923@gmail.com>
2022-10-21 09:12:12 +0200

    Done!!
    Finally finished all of the problems in Problem Set 1 in CS50

--- PSet1/cash.c
+++ PSet1/cash.c
@@ -2,6 +2,10 @@
 
 int get_quarters(int cents);
 int get_cents(void);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+
 
 int main (void)
 {
@@ -10,7 +14,21 @@ int main (void)
     int quarters = get_quarters(cents);
     cents = cents - quarters * 25;
 
-    printf("It is %i quarters, and %i cents. \n", quarters, cents);
+    int dimes = calculate_dimes(cents);
+    cents = cents - (dimes * 10);
+
+    int nickels = calculate_nickels(cents);
+    cents = cents - (nickels * 5);
+
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
+
+    // Sum coins
+    int coins = quarters + dimes + nickels + pennies;
+
+    // Print total number of coins to give the customer
+    printf("%i\n", coins);
+
 }
 
 int get_cents(void)
@@ -24,4 +42,18 @@ int get_quarters(int cents)
 {
     int remainder = cents % 25;
     return (cents - remainder) / 25;
+}
+int calculate_dimes(int cents)
+{
+    int remainder = cents % 10;
+    return (cents - remainder) / 10;
+}
+int calculate_nickels(int cents)
+{
+    int remainder = cents % 5;
+    return (cents - remainder) / 5;
+}
+int calculate_pennies(int cents)
+{
+    return cents;
 }
