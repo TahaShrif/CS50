ea855b98145158ecb0d1c8342bf12b6b4a9465b3
TahaShrif <taha14736923@gmail.com>
2022-10-23 05:12:06 +0200

    First string variable

--- mytesting.c
+++ mytesting.c
@@ -2,12 +2,9 @@
 
 int main(void)
 {
-    // Just saw how arrays are written in W3School
-    // Trying it:
-    int scores[] = {982, 231, 79};
-    for (int i = 0; i < 3; i++)
-    {
-        printf("score number  %i is: %i\n ", i + 1, scores[i]);
-    }
+    // Ok now I'm gonna make the first string here
+
+    char Hi[] = "Hello world";
+    printf("%s", Hi);
     
 }
